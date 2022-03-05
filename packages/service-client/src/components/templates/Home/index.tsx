bef6abdcefc532845f4dd1b51e3075f869b1c8dd
akky-xxxx <114.s.aki@gmail.com>
2022-03-05 22:13:21 +0900

    style(service-client): apply fix script

--- packages/service-client/src/components/templates/Home/index.tsx
+++ packages/service-client/src/components/templates/Home/index.tsx
@@ -16,8 +16,7 @@ export const HomeTemplate: VFC = () => (
       </h1>
 
       <p>
-        Get started by editing{" "}
-        <code>pages/index.tsx</code>
+        Get started by editing <code>pages/index.tsx</code>
       </p>
 
       <div>
@@ -31,16 +30,12 @@ export const HomeTemplate: VFC = () => (
           <p>Learn about Next.js in an interactive course with quizzes!</p>
         </a>
 
-        <a
-          href="https://github.com/vercel/next.js/tree/canary/examples"
-        >
+        <a href="https://github.com/vercel/next.js/tree/canary/examples">
           <h2>Examples &rarr;</h2>
           <p>Discover and deploy boilerplate example Next.js projects.</p>
         </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        >
+        <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
           <h2>Deploy &rarr;</h2>
           <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
         </a>
