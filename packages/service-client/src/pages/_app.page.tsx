2504003f2326890c4b710ff0c034bfcbe88e73ec
akky-xxxx <114.s.aki@gmail.com>
2022-03-06 15:02:54 +0900

    feat(service-client): use Layout

--- packages/service-client/src/pages/_app.page.tsx
+++ packages/service-client/src/pages/_app.page.tsx
@@ -1,12 +1,18 @@
 import type { AppProps } from "next/app"
 import { Fragment } from "react"
 import { GlobalStyle } from "../assets/styles"
+import { Layout } from "../components/layouts/Layout"
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Fragment>
-    <GlobalStyle />
-    <Component {...pageProps} />
-  </Fragment>
-)
+const MyApp = ({ Component, pageProps, router }: AppProps) => {
+  const { route } = router
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <Layout route={route}>
+        <Component {...pageProps} />
+      </Layout>
+    </Fragment>
+  )
+}
 
 export default MyApp
