12dd9e572368dd88693618dc77a6dfa0b05738d3
akky-xxxx <114.s.aki@gmail.com>
2022-03-20 18:58:44 +0900

    feat(service-client): define common style

---
+++ packages/service-client/src/shared/styles/globalStyle/common/index.ts
@@ -0,0 +1,11 @@
+import { css } from "styled-components"
+import { Colors } from "../../../const/Colors"
+
+const { $333333, $fff6e0 } = Colors
+
+export const common = css`
+  body {
+    background-color: ${$fff6e0};
+    color: ${$333333};
+  }
+`
