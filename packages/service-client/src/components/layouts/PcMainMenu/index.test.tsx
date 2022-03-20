79e526762b5c1b93c187f0320e6ca83ecb77ccf2
akky-xxxx <114.s.aki@gmail.com>
2022-03-21 00:36:50 +0900

    test(service-client): implement test for PcMainMenu

---
+++ packages/service-client/src/components/layouts/PcMainMenu/index.test.tsx
@@ -0,0 +1,20 @@
+import { composeStories } from "@storybook/testing-react"
+import { render, screen } from "@testing-library/react"
+import * as stories from "./index.stories"
+
+const { Default, OpenedSubMenu } = composeStories(stories)
+
+describe("PcMainMenu component", () => {
+  it("Default でメニューが表示されていること", () => {
+    render(<Default />)
+    expect(screen.getByText("リンクテキスト1")).toBeTruthy()
+    expect(screen.getByText("リンクテキスト2")).toBeTruthy()
+    expect(screen.getByText("リンクテキスト3")).toBeTruthy()
+  })
+
+  it("OpenedSubMenu でサブメニューが表示されていること", () => {
+    render(<OpenedSubMenu />)
+    expect(screen.getByText("サブメニュー1")).toBeTruthy()
+    expect(screen.getByText("サブメニュー2")).toBeTruthy()
+  })
+})
