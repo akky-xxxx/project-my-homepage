899603a44c24d4c60f37dd631a986dd9d61e762d
akky-xxxx <114.s.aki@gmail.com>
2022-03-20 17:07:57 +0900

    chore(service-client): reset _app to default

--- packages/service-client/src/pages/_app.page.tsx
+++ packages/service-client/src/pages/_app.page.tsx
@@ -1,17 +1,8 @@
 import type { AppProps } from "next/app"
-import { Fragment } from "react"
-import { GlobalStyle } from "../assets/styles"
-import { Layout } from "../components/layouts/Layout"
 
-const MyApp = ({ Component, pageProps, router }: AppProps) => {
-  const { route } = router
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <Fragment>
-      <GlobalStyle />
-      <Layout route={route}>
-        <Component {...pageProps} />
-      </Layout>
-    </Fragment>
+    <Component {...pageProps} />
   )
 }
 
