003ae5cb0e0f6785abcac601b3bd908c68cd3d86
akky-xxxx <114.s.aki@gmail.com>
2022-03-20 21:48:09 +0900

    feat(service-client): implement usePcMainMenu

---
+++ packages/service-client/src/components/layouts/PcMainMenu/modules/usePcMainMenu/index.test.ts
@@ -0,0 +1,39 @@
+import { act, renderHook, RenderHookResult } from "@testing-library/react-hooks"
+import { usePcMainMenu } from "."
+import { HoCPcMainMenuProps } from "../../types"
+
+const usePcMainMenuArguments: HoCPcMainMenuProps = {
+  currentPagePosition: 0,
+  menuData: [
+    {
+      href: "https://www.google.com/",
+      linkText: "リンクテキスト1",
+    },
+    {
+      href: "/href",
+      linkText: "リンクテキスト2",
+    },
+  ],
+}
+
+describe("usePcMainMenu", () => {
+  let renderHookResult: RenderHookResult<
+    undefined,
+    ReturnType<typeof usePcMainMenu>
+  >
+
+  beforeEach(() => {
+    renderHookResult = renderHook(() => usePcMainMenu(usePcMainMenuArguments))
+  })
+
+  it("markerPosition の初期値は 0", () => {
+    expect(renderHookResult.result.current.markerPosition).toBe(0)
+  })
+
+  it("handleHoverMenu に 2 を渡すと markerPosition は 2 になる", () => {
+    act(() => {
+      renderHookResult.result.current.handleHoverMenu(2)
+    })
+    expect(renderHookResult.result.current.markerPosition).toBe(2)
+  })
+})
