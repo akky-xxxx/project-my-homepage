82ba4f6298e88c522ce562b8dbcebd7aa1de8a23
akky-xxxx <114.s.aki@gmail.com>
2022-03-20 19:14:50 +0900

    feat(service-client): define margin const

---
+++ packages/service-client/src/shared/const/Margins/index.ts
@@ -0,0 +1,15 @@
+const Margin4 = 4
+/* eslint-disable no-magic-numbers */
+const Margin8 = Margin4 * 2
+const Margin12 = Margin4 * 3
+const Margin16 = Margin4 * 4
+const Margin20 = Margin4 * 5
+/* eslint-enable */
+
+export const Margins = {
+  Margin4,
+  Margin8,
+  Margin12,
+  Margin16,
+  Margin20,
+} as const
