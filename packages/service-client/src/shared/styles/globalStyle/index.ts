8450c73cb21976340753d67821559e5c367ab15b
akky-xxxx <114.s.aki@gmail.com>
2022-03-20 18:59:02 +0900

    feat(service-client): add common style to global style

--- packages/service-client/src/shared/styles/globalStyle/index.ts
+++ packages/service-client/src/shared/styles/globalStyle/index.ts
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from "styled-components"
+import { common } from "./common"
 import { reset } from "./reset"
 
 export const GlobalStyle = createGlobalStyle`
 ${reset};
+${common};
 `
