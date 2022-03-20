09d6c2a340d61e4e75487750797052733cf6f1bb
akky-xxxx <114.s.aki@gmail.com>
2022-03-20 23:53:29 +0900

    feat(service-client): remove background color

--- packages/service-client/src/components/layouts/PcMenuListItem/index.tsx
+++ packages/service-client/src/components/layouts/PcMenuListItem/index.tsx
@@ -1,10 +1,7 @@
 import styled from "styled-components"
-import { Colors } from "../../../shared/const/Colors"
 
-const { $ffffff } = Colors
 
 export const PcMenuListItem = styled.li`
-  background-color: ${$ffffff};
   flex-grow: 1;
   position: relative;
 
