2f611dd69d10f97d08f452b0c29994e77231b815
akky-xxxx <114.s.aki@gmail.com>
2022-03-20 23:57:39 +0900

    feat(service-client): implement AnchorBack

---
+++ packages/service-client/src/components/layouts/AnchorBack/index.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+import { Colors } from "../../../shared/const/Colors"
+
+const { $ffffff } = Colors
+
+export const AnchorBack = styled.div`
+  background-color: ${$ffffff};
+`
