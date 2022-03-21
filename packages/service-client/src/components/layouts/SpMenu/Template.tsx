c9636d23778a380e3cd4898776bcda8e0b505304
akky-xxxx <114.s.aki@gmail.com>
2022-03-21 22:32:01 +0900

    style(service-client): apply fix task

--- packages/service-client/src/components/layouts/SpMenu/Template.tsx
+++ packages/service-client/src/components/layouts/SpMenu/Template.tsx
@@ -1,10 +1,10 @@
 import { Fragment, VFC } from "react"
-import { SpMenuBackground } from "./components/atoms/SpMenuBackground"
-import { SpMenuButton } from "./components/atoms/SpMenuButton"
-import { SpMenuItems } from "./components/atoms/SpMenuItems"
 import { Menu } from "./components/atoms/Menu"
+import { SpMenuBackground } from "./components/atoms/SpMenuBackground"
 import { SpMenuBackgroundWrapper } from "./components/atoms/SpMenuBackgroundWrapper"
+import { SpMenuButton } from "./components/atoms/SpMenuButton"
 import { SpMenuButtonWrapper } from "./components/atoms/SpMenuButtonWrapper"
+import { SpMenuItems } from "./components/atoms/SpMenuItems"
 import { SpMenuItemsWrapper } from "./components/atoms/SpMenuItemsWrapper"
 import { SpMenuProps } from "./types"
 
