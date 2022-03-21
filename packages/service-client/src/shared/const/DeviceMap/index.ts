dd49fb27d623c9678ceacbeb7702acf28180fe13
akky-xxxx <114.s.aki@gmail.com>
2022-03-22 00:34:25 +0900

    feat(service-client): define DeviceMap const

---
+++ packages/service-client/src/shared/const/DeviceMap/index.ts
@@ -0,0 +1,5 @@
+// refer /node_modules/puppeteer-core/lib/cjs/puppeteer/common/DeviceDescriptors.js
+export const DeviceMap = {
+  ipad: "iPad",
+  pc: "iPad Pro landscape", // pc がわからないため代用
+} as const
