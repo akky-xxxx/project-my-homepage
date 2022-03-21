25ce86a5d303b718fa0e1a5e7f286d274da6ae16
akky-xxxx <114.s.aki@gmail.com>
2022-03-21 19:06:42 +0900

    feat(service-client): implement SpMenuItems

---
+++ packages/service-client/src/components/layouts/SpMenuItems/index.stories.tsx
@@ -0,0 +1,54 @@
+import { ComponentStoryObj, Meta } from "@storybook/react"
+import { ComponentProps } from "react"
+import { SpMenuItems } from "./index"
+
+type SpMenuItemsType = typeof SpMenuItems
+type SpMenuItemsStory = ComponentStoryObj<SpMenuItemsType>
+
+const meta: Meta<ComponentProps<SpMenuItemsType>> = {
+  args: {
+    menu: [
+      {
+        href: "/href",
+        linkText: "リンクテキスト1",
+        subMenu: [
+          {
+            href: "/?1",
+            linkText: "サブメニュー1",
+          },
+          {
+            href: "/?2",
+            linkText: "サブメニュー2",
+          },
+        ],
+      },
+      {
+        href: "https://www.google.com/?1",
+        linkText: "リンクテキスト2",
+      },
+      {
+        href: "https://www.google.com/?2",
+        linkText: "リンクテキスト3",
+      },
+    ],
+  },
+  component: SpMenuItems,
+  parameters: {
+    backgrounds: {
+      default: "blue",
+    },
+  },
+}
+export default meta
+
+export const Opened: SpMenuItemsStory = {
+  args: {
+    isOpened: true,
+  },
+}
+
+export const Closed: SpMenuItemsStory = {
+  args: {
+    isOpened: false,
+  },
+}
---
+++ packages/service-client/src/components/layouts/SpMenuItems/index.tsx
@@ -0,0 +1,53 @@
+import { VFC } from "react"
+import styled from "styled-components"
+import { Menu } from "../../../shared/const/Menu"
+import { Anchor } from "../../atoms/Anchor"
+import { MainMenuLi } from "./components/atoms/MainMenuLi"
+import { MainMenuLinkText } from "./components/atoms/MainMenuLinkText"
+import { MainMenuUl } from "./components/atoms/MainMenuUl"
+import { SpSubMenuItems } from "./components/atoms/SpSubMenuItems"
+
+type MenuWithSubMenu = Menu & {
+  subMenu?: Menu[]
+}
+
+type SpMenuItemsProps = {
+  isOpened: boolean
+  menu: MenuWithSubMenu[]
+}
+
+export const SpMenuItems: VFC<SpMenuItemsProps> = (props) => {
+  const { isOpened, menu } = props
+
+  return (
+    <Wrapper className={isOpened ? "is-opened" : ""}>
+      <MainMenuUl>
+        {menu.map((menuData) => {
+          const { href: menuHref, linkText: menuLinkText, subMenu } = menuData
+
+          return (
+            <MainMenuLi key={menuHref}>
+              <Anchor href={menuHref}>
+                <MainMenuLinkText>{menuLinkText}</MainMenuLinkText>
+              </Anchor>
+
+              {subMenu && <SpSubMenuItems subMenu={subMenu} />}
+            </MainMenuLi>
+          )
+        })}
+      </MainMenuUl>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  transition: opacity 0.2s;
+  opacity: 0;
+  pointer-events: none;
+
+  &.is-opened {
+    opacity: 1;
+    pointer-events: inherit;
+    transition-delay: 0.3s;
+  }
+`
