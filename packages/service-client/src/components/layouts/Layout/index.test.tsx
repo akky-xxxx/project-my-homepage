1fc38a1e867162b99678f9132166f5269b7cb2d6
akky-xxxx <114.s.aki@gmail.com>
2022-03-22 00:51:42 +0900

    test(service-client): adjust test

--- packages/service-client/src/components/layouts/Layout/index.test.tsx
+++ packages/service-client/src/components/layouts/Layout/index.test.tsx
@@ -2,9 +2,17 @@ import { composeStories } from "@storybook/testing-react"
 import { render, screen } from "@testing-library/react"
 import * as stories from "./index.stories"
 
-const { Default } = composeStories(stories)
+const { Pc, Tablet } = composeStories(stories)
 
-it("Layout component", () => {
-  render(<Default />)
-  expect(screen.getByText("test")).toBeTruthy()
+// TODO: 意味のあるテストに変える
+describe("Layout component", () => {
+  it("Pc", () => {
+    render(<Pc />)
+    expect(screen.getByText("子要素")).toBeVisible()
+  })
+
+  it("Tablet", () => {
+    render(<Tablet />)
+    expect(screen.getByText("子要素")).toBeVisible()
+  })
 })
