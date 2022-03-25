d57c3c0a18faed5c5a1fdcb3105161f8eeaddbf3
akky-xxxx <114.s.aki@gmail.com>
2022-03-25 22:53:48 +0900

    feat(service-client): adjust position of menu

--- packages/service-client/src/components/layouts/SpMenu/components/atoms/SpMenuItemsWrapper/index.tsx
+++ packages/service-client/src/components/layouts/SpMenu/components/atoms/SpMenuItemsWrapper/index.tsx
@@ -3,6 +3,7 @@ import { ZIndexes } from "../../../const/ZIndexes"
 
 export const SpMenuItemsWrapper = styled.div`
   left: 0;
+  padding-top: 20px;
   position: fixed;
   right: 0;
   z-index: ${ZIndexes.Items};
