e61e89fd2fec9a66f6e3b7e3eeac9a26460e6ad3
akky-xxxx <114.s.aki@gmail.com>
2022-03-29 21:55:48 +0900

    chore(common-icons): adjust index template
    for additional export that existed

--- packages/common-icons/configs/svgr/indexTemplate.js
+++ packages/common-icons/configs/svgr/indexTemplate.js
@@ -1,12 +1,25 @@
 const path = require("path")
+const fs = require("fs")
+
+const DistDir = path.join(__dirname, "../../", "dist")
+
+const ignoreTsExtensions = (fileName) => fileName.endsWith(".tsx")
+const replaceExtensions = (fileName) => fileName.replace(".tsx", "")
+const getExport = (componentName) =>
+  `export { ${componentName} } from './${componentName}'`
 
 const indexTemplate = (filePaths) => {
+  const existedIcons = fs
+    .readdirSync(DistDir)
+    .filter(ignoreTsExtensions)
+    .map(replaceExtensions)
+    .map(getExport)
   const exportEntries = filePaths.map((filePath) => {
     const basename = path.basename(filePath, path.extname(filePath))
     const exportName = /^\d/.test(basename) ? `Svg${basename}` : basename
     return `export { ${exportName} } from './${basename}'`
   })
-  return exportEntries.join("\n")
+  return [...new Set([...exportEntries, ...existedIcons])].sort().join("\n")
 }
 
 exports.indexTemplate = indexTemplate
