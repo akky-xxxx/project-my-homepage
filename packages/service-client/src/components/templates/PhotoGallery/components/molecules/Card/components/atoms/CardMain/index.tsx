2f8b00366752a20b3690ff333f94eb390e891f9b
akky-xxxx <114.s.aki@gmail.com>
2022-03-29 22:03:48 +0900

    feat(service-client): adjust reversed style

--- packages/service-client/src/components/templates/PhotoGallery/components/molecules/Card/components/atoms/CardMain/index.tsx
+++ packages/service-client/src/components/templates/PhotoGallery/components/molecules/Card/components/atoms/CardMain/index.tsx
@@ -1,5 +1,8 @@
 import styled from "styled-components"
 import { MediaQuery } from "../../../../../../../../../shared/const/MediaQuery"
+import { Classes } from "../../../const/Classes"
+
+const { IsShownDetail } = Classes
 
 export const CardMain = styled.div`
   display: inline-block;
@@ -15,8 +18,18 @@ export const CardMain = styled.div`
 
   /* PC の時のみ hover にすると story.hover で再現しなかったため */
   ${MediaQuery.Tb} {
+    &:hover {
+      figure {
+        transform: none;
+      }
+    }
+  }
+
+  /* stylelint-disable-next-line selector-class-pattern */
+  &.${IsShownDetail} {
+    /* stylelint-disable-next-line no-descending-specificity */
     figure {
-      transform: none !important;
+      transform: rotateY(0.5turn);
     }
   }
 `
