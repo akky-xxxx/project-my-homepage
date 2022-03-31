0c00199d72b24e30306062318f2ddea19307b38e
akky-xxxx <114.s.aki@gmail.com>
2022-03-31 23:04:22 +0900

    feat(service-client): implement Tag

---
+++ packages/service-client/src/components/atoms/Tag/index.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components"
+import { Colors } from "../../../shared/const/Colors"
+
+const { $003989, $ffffff, $333333 } = Colors
+
+type TagProps = {
+  tagColor?: string
+  isLighterTest?: boolean
+}
+
+type GetCssValue = (props: TagProps) => string
+const getColorValue: GetCssValue = (props) => props.tagColor || $003989
+const getTextColorValue: GetCssValue = (props) => {
+  const { isLighterTest = true } = props
+  return isLighterTest ? $ffffff : $333333
+}
+
+export const Tag = styled.span<TagProps>`
+  background-color: ${getColorValue};
+  border-radius: 5px;
+  color: ${getTextColorValue};
+  display: inline-block;
+  padding: 4px 8px;
+  transition: opacity 0.3s;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`
