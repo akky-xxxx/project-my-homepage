33476fc2d4b9a2b12db473f84081ca7c42c45f11
akky-xxxx <114.s.aki@gmail.com>
2022-04-02 23:35:35 +0900

    feat(service-client): use getCurrentPagePosition

--- packages/service-client/src/pages/_app.page.tsx
+++ packages/service-client/src/pages/_app.page.tsx
@@ -3,14 +3,21 @@ import { Fragment } from "react"
 import { Layout } from "../components/layouts/Layout"
 import { MenuData } from "../shared/const/MenuData"
 import { GlobalStyle } from "../shared/styles/globalStyle"
+import { getCurrentPagePosition } from "./modules/getCurrentPagePosition"
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Fragment>
-    <GlobalStyle />
-    <Layout menu={MenuData} currentPagePosition={0}>
-      <Component {...pageProps} />
-    </Layout>
-  </Fragment>
-)
+const MyApp = ({ Component, pageProps, router }: AppProps) => {
+  const { route } = router
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <Layout
+        menu={MenuData}
+        currentPagePosition={getCurrentPagePosition(route)}
+      >
+        <Component {...pageProps} />
+      </Layout>
+    </Fragment>
+  )
+}
 
 export default MyApp
