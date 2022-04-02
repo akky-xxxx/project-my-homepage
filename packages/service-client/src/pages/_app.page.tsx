b3f681b16db3a811f266c10be93d9d38b89475fd
akky-xxxx <114.s.aki@gmail.com>
2022-04-02 21:02:43 +0900

    feat(service-client): build in Layout into _app

--- packages/service-client/src/pages/_app.page.tsx
+++ packages/service-client/src/pages/_app.page.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from "next/app"
 import { Fragment } from "react"
+import { Layout } from "../components/layouts/Layout"
 import { GlobalStyle } from "../shared/styles/globalStyle"
+import { MenuData } from "../shared/const/MenuData/indedx"
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Fragment>
     <GlobalStyle />
-    <Component {...pageProps} />
+    <Layout menu={MenuData} currentPagePosition={0}>
+      <Component {...pageProps} />
+    </Layout>
   </Fragment>
 )
 
