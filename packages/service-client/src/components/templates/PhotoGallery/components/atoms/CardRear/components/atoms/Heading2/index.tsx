ff45df9f70fdc83e65615b79f76234cafd5052ad
akky-xxxx <114.s.aki@gmail.com>
2022-04-02 16:17:08 +0900

    feat(service-client): implement components of CardRear

---
+++ packages/service-client/src/components/templates/PhotoGallery/components/atoms/CardRear/components/atoms/Heading2/index.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+import { FontSizes } from "../../../../../../../../../shared/const/FontSizes"
+import { FontWeights } from "../../../../../../../../../shared/const/FontWeights"
+
+const { px24 } = FontSizes
+
+export const Heading2 = styled.h2`
+  font-size: ${px24};
+  font-weight: ${FontWeights.Bold};
+`
---
+++ packages/service-client/src/components/templates/PhotoGallery/components/atoms/CardRear/components/atoms/TakenInfo/index.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+import { MediaQuery } from "../../../../../../../../../shared/const/MediaQuery"
+
+export const TakenInfo = styled.section`
+  ${MediaQuery.Pc} {
+    text-align: right;
+  }
+`
