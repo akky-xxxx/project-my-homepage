8592a0d4eb6341b5a68da7c5b7b95987d88b3f96
akky-xxxx <114.s.aki@gmail.com>
2022-04-02 19:21:43 +0900

    feat(service-client): implement getEmptyArray

---
+++ packages/service-client/src/shared/utils/getEmptyArray/index.test.ts
@@ -0,0 +1,14 @@
+import { getEmptyArray } from "."
+
+describe("getEmptyArray", () => {
+  it("3を渡すと length 3の配列を返す", () => {
+    expect(getEmptyArray(3)).toHaveLength(3)
+  })
+
+  it("要素は全て undefined", () => {
+    const array = getEmptyArray(3)
+    expect(array[0]).toBeUndefined()
+    expect(array[1]).toBeUndefined()
+    expect(array[2]).toBeUndefined()
+  })
+})
