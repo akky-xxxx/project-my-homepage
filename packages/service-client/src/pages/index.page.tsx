3b16fab748ad1b6923dc02478c4fff0cad3bd3bc
akky-xxxx <114.s.aki@gmail.com>
2022-04-03 21:27:29 +0900

    feat(service-client): replace to fetthing to bff

--- packages/service-client/src/pages/index.page.tsx
+++ packages/service-client/src/pages/index.page.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps, NextPage } from "next"
 import { HomeTemplate } from "../components/templates/Home"
+import { bffApiClient } from "../shared/utils/bffApiClient"
 
 type HomePageProps = {
   mainVisualPaths: string[]
@@ -10,21 +11,12 @@ const HomePage: NextPage<HomePageProps> = (props) => {
   return <HomeTemplate mainVisualPaths={mainVisualPaths} />
 }
 
-// TODO: 非同期処理を組み込んだら disable を消す
-// eslint-disable-next-line @typescript-eslint/require-await
-export const getStaticProps: GetStaticProps<HomePageProps> = async () =>
-  // TODO: bff の endpoint を叩いた結果に置き換える
-  ({
-    props: {
-      mainVisualPaths: [
-        "http://placehold.jp/3d4070/ffffff/2000x1000.png?text=image1",
-        "http://placehold.jp/3d4070/ffffff/2000x1000.png?text=image2",
-        "http://placehold.jp/3d4070/ffffff/2000x1000.png?text=image3",
-        "http://placehold.jp/3d4070/ffffff/2000x1000.png?text=image4",
-        "http://placehold.jp/3d4070/ffffff/2000x1000.png?text=image5",
-        "http://placehold.jp/3d4070/ffffff/2000x1000.png?text=image6",
-      ],
-    },
-  })
+export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
+  const { data: result } = await bffApiClient.get<HomePageProps>("/home")
+
+  return {
+    props: result,
+  }
+}
 
 export default HomePage
