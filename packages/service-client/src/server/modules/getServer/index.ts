bd4c646b854b44220eddd037d46b493cdb480c7f
akky-xxxx <114.s.aki@gmail.com>
2022-04-03 17:36:37 +0900

    feat(service-client): use controller

--- packages/service-client/src/server/modules/getServer/index.ts
+++ packages/service-client/src/server/modules/getServer/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express"
 import { RequestHandler } from "next/dist/server/next"
+import { router } from "../../controllers"
 import { errorHandler } from "../errorHandler"
 import { middleware } from "../middleware"
 
@@ -9,6 +10,8 @@ export const getServer: GetServer = (nextHandler) => {
   const server = express()
 
   server.use(middleware)
+
+  server.use(router)
 
   if (nextHandler) {
     server.all("*", (req, res) => {
