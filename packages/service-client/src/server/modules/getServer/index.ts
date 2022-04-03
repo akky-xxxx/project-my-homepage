85d1ae5e4f8d6ef74840561119a4ccd245615c2a
akky-xxxx <114.s.aki@gmail.com>
2022-04-03 15:14:21 +0900

    feat(service-client): implement getServer

---
+++ packages/service-client/src/server/modules/getServer/index.ts
@@ -0,0 +1,23 @@
+import express, { Express } from "express"
+import { RequestHandler } from "next/dist/server/next"
+import { errorHandler } from "../errorHandler"
+import { middleware } from "../middleware"
+
+// test 向けに nextHandler を optional にしてる
+type GetServer = (nextHandler?: RequestHandler) => Express
+export const getServer: GetServer = (nextHandler) => {
+  const server = express()
+
+  server.use(middleware)
+
+  if (nextHandler) {
+    server.all("*", (req, res) => {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      nextHandler(req, res)
+    })
+  }
+
+  server.use(errorHandler)
+
+  return server
+}
