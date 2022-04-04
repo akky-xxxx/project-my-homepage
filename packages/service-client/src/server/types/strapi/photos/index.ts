230ab2e7d3e63e229c1bae18841c226870081d06
akky-xxxx <114.s.aki@gmail.com>
2022-04-05 00:07:55 +0900

    refactor(service-client): replace to base scheme types

--- packages/service-client/src/server/types/strapi/photos/index.ts
+++ packages/service-client/src/server/types/strapi/photos/index.ts
@@ -1,42 +1,13 @@
-import { Dates, StrapiItem, StrapiResponse } from "../common"
+import { LocationBase, PhotoBase, TagBase } from "../baseScheme"
+import { StrapiItem, StrapiResponse } from "../common"
 
-type Rectangle = Record<"height" | "width", number>
-
-type PhotoFormatTypes = "large" | "medium" | "small" | "thumbnail"
-
-type PhotoFormat = Rectangle & {
-  name: string
-  hash: string
-  ext: string
-  mime: string
-  path: null
-  size: number
-  url: string
-}
-
-type PhotoAttributes = Omit<Dates, "publishedAt"> &
-  Rectangle & {
-    name: string
-    alternativeText: string
-    caption: string
-    formats: Record<PhotoFormatTypes, PhotoFormat>
-    ext: string
-    mine: string
-    size: string
-    url: string
+type Photo = PhotoBase & {
+  tags: {
+    data: StrapiItem<TagBase>[]
   }
-
-export type PhotoBase = Dates & {
-  photo: {
-    data: StrapiItem<PhotoAttributes>
+  location: {
+    data: StrapiItem<LocationBase>
   }
-  takenAt: string
-  isShownTop: boolean
 }
 
-type Attributes = PhotoBase & {
-  tags: unknown
-  location: unknown
-}
-
-export type Photos = StrapiResponse<StrapiItem<Attributes>[]>
+export type Photos = StrapiResponse<StrapiItem<Photo>[]>
