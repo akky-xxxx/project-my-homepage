2786b3a2730df5bcc7452d09db5e1f91ecaca086
akky-xxxx <114.s.aki@gmail.com>
2022-04-09 15:30:01 +0900

    refactor(service-client): split file

--- packages/service-client/src/server/handlers/homeGet/index.test.ts
+++ packages/service-client/src/server/handlers/homeGet/index.test.ts
@@ -1,41 +1,14 @@
 import supertest from "supertest"
-import { AxiosResponse } from "axios"
 import { spyOn } from "jest-mock"
 // node_modules を除き最初に import させるため
 // organize-imports-ignore
 import "../../modules/initialize"
 import { strapiApiClient } from "../../utils/strapiApiClient"
 import { getServer } from "../../modules/getServer"
-import { Photos } from "../../types/strapi/photos"
+import { spyResponse } from "./const/spy"
 
 const app = getServer()
 const request = supertest(app)
-
-const spyResponse: AxiosResponse<Photos> = {
-  status: 200,
-  data: {
-    data: [
-      {
-        attributes: {
-          photo: {
-            data: {
-              attributes: {
-                formats: {
-                  // データを完全再現するのが手間なため
-                  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                  // @ts-ignore
-                  large: {
-                    url: "http://placehold.jp/3d4070/ffffff/2000x1000.png?text=image1",
-                  },
-                },
-              },
-            },
-          },
-        },
-      },
-    ],
-  },
-}
 
 describe("GET /api/home", () => {
   it("home 取得成功時、status: 200 でフロント用のデータを返す", async () => {
