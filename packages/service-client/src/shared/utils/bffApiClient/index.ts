ab821b7b50713a090e71afb3832572226fc6d284
akky-xxxx <114.s.aki@gmail.com>
2022-04-17 13:38:15 +0900

    style(service-client): apply fix task
    https://github.com/akky-xxxx/project-my-homepage/issues/31

--- packages/service-client/src/shared/utils/bffApiClient/index.ts
+++ packages/service-client/src/shared/utils/bffApiClient/index.ts
@@ -2,7 +2,8 @@ import axios from "axios"
 
 const { NEXT_PUBLIC_SG_SERVER } = process.env
 
-if (!NEXT_PUBLIC_SG_SERVER) throw new Error("NEXT_PUBLIC_SG_SERVER が定義されてません")
+if (!NEXT_PUBLIC_SG_SERVER)
+  throw new Error("NEXT_PUBLIC_SG_SERVER が定義されてません")
 
 export const bffApiClient = axios.create({
   baseURL: `${NEXT_PUBLIC_SG_SERVER}/api`,
