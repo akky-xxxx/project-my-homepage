fbd9e865e0f0c06544f14dbde130834edaa8beeb
akky-xxxx <114.s.aki@gmail.com>
2022-04-30 22:36:01 +0900

    feat(terraform): setup cloud run as temporary
    https://github.com/akky-xxxx/project-my-homepage/issues/42

---
+++ packages/terraform/src/main.tf
@@ -0,0 +1,136 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("${var.credentials}.json")
+
+  project = var.project_id
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_cloud_run_service" "client" {
+  name     = lookup(var.run_client, "name")
+  location = var.region
+
+  template {
+    spec {
+      container_concurrency = lookup(var.run_client, "concurrency")
+
+      containers {
+        image = lookup(var.run_client, "registry")
+
+        resources {
+          limits = {
+            memory = lookup(var.run_client, "memory")
+            cpu : lookup(var.run_client, "cpu")
+          }
+        }
+
+        env {
+          name  = "TEST"
+          value = lookup(var.run_client, "env_test")
+        }
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_service" "sg_server" {
+  name     = lookup(var.run_sg_server, "name")
+  location = var.region
+
+  metadata {
+    annotations = {
+      "run.googleapis.com/ingress" = "internal"
+    }
+  }
+
+  template {
+    spec {
+      container_concurrency = lookup(var.run_sg_server, "concurrency")
+
+      containers {
+        image = lookup(var.run_sg_server, "registry")
+
+        resources {
+          limits = {
+            memory = lookup(var.run_sg_server, "memory")
+            cpu : lookup(var.run_sg_server, "cpu")
+          }
+        }
+
+        env {
+          name  = "TEST"
+          value = lookup(var.run_sg_server, "env_test")
+        }
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_service" "strapi" {
+  name     = lookup(var.run_strapi, "name")
+  location = var.region
+
+  template {
+    spec {
+      container_concurrency = lookup(var.run_strapi, "concurrency")
+
+      containers {
+        image = lookup(var.run_strapi, "registry")
+
+        resources {
+          limits = {
+            memory = lookup(var.run_strapi, "memory")
+            cpu : lookup(var.run_strapi, "cpu")
+          }
+        }
+
+        env {
+          name  = "TEST"
+          value = lookup(var.run_strapi, "env_test")
+        }
+      }
+    }
+  }
+}
+
+data "google_iam_policy" "no_auth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "no_auth-client" {
+  location = google_cloud_run_service.client.location
+  project  = google_cloud_run_service.client.project
+  service  = google_cloud_run_service.client.name
+
+  policy_data = data.google_iam_policy.no_auth.policy_data
+}
+
+resource "google_cloud_run_service_iam_policy" "no_auth-sg_server" {
+  location = google_cloud_run_service.sg_server.location
+  project  = google_cloud_run_service.sg_server.project
+  service  = google_cloud_run_service.sg_server.name
+
+  policy_data = data.google_iam_policy.no_auth.policy_data
+}
+
+resource "google_cloud_run_service_iam_policy" "no_auth-strapi" {
+  location = google_cloud_run_service.strapi.location
+  project  = google_cloud_run_service.strapi.project
+  service  = google_cloud_run_service.strapi.name
+
+  policy_data = data.google_iam_policy.no_auth.policy_data
+}
---
+++ packages/terraform/src/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {}
+variable "credentials" {}
+variable "project_id" {}
+
+variable "run_client" {}
+variable "run_sg_server" {}
+variable "run_strapi" {}
