81614abe8514c34ca2a4d6f046ddab58ee8b29f9
akky-xxxx <114.s.aki@gmail.com>
2022-05-07 21:45:44 +0900

    feat(service-sg-server): join tagsGet to server
    https://github.com/akky-xxxx/project-my-homepage/issues/43

--- packages/service-sg-server/src/controllers/apiRouter/index.ts
+++ packages/service-sg-server/src/controllers/apiRouter/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import { homeGet } from "../../handlers/homeGet"
 import { photoGalleryGet } from "../../handlers/photoGalleryGet"
+import { tagsGet } from "../../handlers/tagsGet"
 
 const apiRouter = Router()
 
 apiRouter.get("/home", homeGet)
 apiRouter.get("/photo-gallery", photoGalleryGet)
+apiRouter.get("/tags", tagsGet)
 
 export { apiRouter }
