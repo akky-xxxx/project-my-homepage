6b0bd916158844e95906ea943be9b9ad7909c18a
akky-xxxx <114.s.aki@gmail.com>
2022-05-07 22:49:28 +0900

    test(service-sg-server): implement test of locationsGet
    https://github.com/akky-xxxx/project-my-homepage/issues/44

---
+++ packages/service-sg-server/src/handlers/locationsGet/index.test.ts
@@ -0,0 +1,88 @@
+import { UnknownObject } from "common-types"
+import supertest, { Response } from "supertest"
+// node_modules を除き最初に import させるため
+// organize-imports-ignore
+import "../../modules/initialize"
+import { spyOn } from "jest-mock"
+import { getServer } from "../../modules/getServer"
+import { strapiApiClient } from "../../shared/utils/strapiApiClient"
+import { mockFunction } from "./const/spy"
+
+const app = getServer()
+const request = supertest(app)
+
+describe("GET /api/locations", () => {
+  describe("locations 200 パターン", () => {
+    let spy: ReturnType<typeof spyOn>
+    let res: Response
+
+    beforeEach(async () => {
+      spy = spyOn(strapiApiClient, "get").mockImplementation(mockFunction)
+      res = await request.get("/api/locations")
+    })
+
+    afterEach(() => {
+      spy.mockRestore()
+    })
+
+    it("200 を返すこと", () => {
+      expect(spy).toHaveBeenCalled()
+      expect(res.status).toBe(200)
+    })
+
+    it("locations プロパティがあり、配列である", () => {
+      expect(spy).toHaveBeenCalled()
+      expect(Array.isArray(res.body?.locations)).toBeTruthy()
+    })
+
+    it("locations[number] はオブジェクトである", () => {
+      expect(spy).toHaveBeenCalled()
+      if (!Array.isArray(res.body?.locations)) return
+      res.body.locations.forEach((location: unknown) => {
+        expect(typeof location).toBe("object")
+        expect(location).not.toBeNull()
+      })
+    })
+
+    it("locations[number].locationName は文字列である", () => {
+      expect(spy).toHaveBeenCalled()
+      res.body.locations.forEach((location: unknown) => {
+        if (typeof location !== "object" || location === null) return
+        const newLocation: UnknownObject = { ...location }
+
+        expect(typeof newLocation.locationName).toBe("string")
+      })
+    })
+
+    it("locations[number].locationId は文字列である", () => {
+      expect(spy).toHaveBeenCalled()
+      res.body.locations.forEach((location: unknown) => {
+        if (typeof location !== "object" || location === null) return
+        const newLocation: UnknownObject = { ...location }
+
+        expect(typeof newLocation.locationId).toBe("string")
+      })
+    })
+
+    it("locations[number].imagePath は http で始まる文字列である", () => {
+      expect(spy).toHaveBeenCalled()
+      res.body.locations.forEach((location: unknown) => {
+        if (typeof location !== "object" || location === null) return
+        const newLocation: UnknownObject = { ...location }
+
+        if (typeof newLocation.imagePath !== "string") return
+        expect(newLocation.imagePath.startsWith("http")).toBe(true)
+      })
+    })
+
+    it("locations[number].imageNun は数字である", () => {
+      expect(spy).toHaveBeenCalled()
+      res.body.locations.forEach((location: unknown) => {
+        if (typeof location !== "object" || location === null) return
+        const newLocation: UnknownObject = { ...location }
+
+        expect(typeof newLocation.imageNum).toBe("number")
+      })
+    })
+  })
+})
