efa1f4ffc7d2ff7740180489ab23e6faf4743104
akky-xxxx <114.s.aki@gmail.com>
2022-05-07 21:34:20 +0900

    feat(service-sg-server): implement modules of tagsGet
    https://github.com/akky-xxxx/project-my-homepage/issues/43

---
+++ packages/service-sg-server/src/handlers/tagsGet/modules/extractImagePath/index.ts
@@ -0,0 +1,14 @@
+import { Photos } from "common-types"
+import { extractPhotoThumbnailUrl } from "./modules/extractPhotoThumbnailUrl"
+import { isSamePhotoId } from "./modules/isSamePhotoId"
+
+export type ExtractImagePathMain = (targetPhotoId: number) => string
+type ExtractImagePath = (photoList: Photos["data"]) => ExtractImagePathMain
+
+export const extractImagePath: ExtractImagePath =
+  (photoList) => (targetPhotoId) => {
+    const isSamePhotoIdMain = isSamePhotoId(targetPhotoId)
+    const targetPhoto = photoList.find(isSamePhotoIdMain)
+    if (!targetPhoto) return ""
+    return extractPhotoThumbnailUrl(targetPhoto)
+  }
---
+++ packages/service-sg-server/src/handlers/tagsGet/modules/extractImagePath/modules/extractPhotoThumbnailUrl/index.ts
@@ -0,0 +1,6 @@
+import { Photos } from "common-types"
+
+type ExtractPhotoThumbnailUrl = (photo: Photos["data"][number]) => string
+
+export const extractPhotoThumbnailUrl: ExtractPhotoThumbnailUrl = (photo) =>
+  photo.attributes.photo.data.attributes.formats.thumbnail.url
