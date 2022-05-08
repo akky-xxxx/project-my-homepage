68b8e46433ffc6c80afaf5f0f7bbc6a092c5e46d
akky-xxxx <114.s.aki@gmail.com>
2022-05-08 19:40:57 +0900

    feat(service-sg-server): join locationsIdGet to server
    https://github.com/akky-xxxx/project-my-homepage/issues/65

--- packages/service-sg-server/src/controllers/apiRouter/index.ts
+++ packages/service-sg-server/src/controllers/apiRouter/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import { homeGet } from "../../handlers/homeGet"
 import { locationsGet } from "../../handlers/locationsGet"
+import { locationsIdGet } from "../../handlers/locationsIdGet"
 import { photoGalleryGet } from "../../handlers/photoGalleryGet"
 import { tagsGet } from "../../handlers/tagsGet"
 import { tagsIdGet } from "../../handlers/tagsIdGet"
@@ -9,6 +10,7 @@ const apiRouter = Router()
 
 apiRouter.get("/home", homeGet)
 apiRouter.get("/locations", locationsGet)
+apiRouter.get("/locations/:locationId", locationsIdGet)
 apiRouter.get("/photo-gallery", photoGalleryGet)
 apiRouter.get("/tags", tagsGet)
 apiRouter.get("/tags/:tagId", tagsIdGet)
