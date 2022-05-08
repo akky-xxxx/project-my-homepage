a7291a8ca825d31c44e62539fb4a79cef72c02a2
akky-xxxx <114.s.aki@gmail.com>
2022-05-08 18:02:27 +0900

    feat(service-sg-server): implement getTargetTags
    https://github.com/akky-xxxx/project-my-homepage/issues/64

---
+++ packages/service-sg-server/src/handlers/tagsIdGet/modules/getClientImage/modules/getTargetTags/index.ts
@@ -0,0 +1,18 @@
+import { Photos, Tags } from "common-types"
+import { TagsIdGETRes } from "../../../../../../libs/bffApiClient"
+import { hasSameId } from "../../../../../../shared/utils/hasSameId"
+
+type GetTargetTagsMain = (
+  relatedTag: Photos["data"][number]["attributes"]["tags"]["data"][number],
+) => TagsIdGETRes["images"][number]["tags"][number]
+type GetTargetTags = (tags: Tags["data"]) => GetTargetTagsMain
+
+export const getTargetTags: GetTargetTags = (tags) => (relatedTag) => {
+  const { id } = relatedTag
+  const hasSameTagId = hasSameId(id)
+
+  return {
+    tagId: String(id),
+    tagName: tags.find(hasSameTagId)?.attributes.tagName || "",
+  }
+}
