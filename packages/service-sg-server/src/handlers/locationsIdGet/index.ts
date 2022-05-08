fd8e4837012dfe7889b96c175ea2d3672da4305a
akky-xxxx <114.s.aki@gmail.com>
2022-05-08 21:19:03 +0900

    refactor(service-sg-server): replace to strapiApiMethods at locationsIdGet
    https://github.com/akky-xxxx/project-my-homepage/issues/92

--- packages/service-sg-server/src/handlers/locationsIdGet/index.ts
+++ packages/service-sg-server/src/handlers/locationsIdGet/index.ts
@@ -1,19 +1,14 @@
-import { LocationsId, Photos, Tags } from "common-types"
 import { Request } from "express"
 import { LocationsIdGETRes } from "../../libs/bffApiClient"
-import { Query } from "../../shared/const/strapi/Query"
 import { apiHandler } from "../../shared/utils/apiHandler"
-import { isErrorStatus } from "../../shared/utils/isErrorStatus"
 import { infoLogger, loggerWrapper } from "../../shared/utils/logger"
-import { strapiApiClient } from "../../shared/utils/strapiApiClient"
+import { strapiApiMethods } from "../../shared/utils/strapiApiMethods"
 import { getClientImage } from "./modules/getClientImage"
 
 type TagsIdGetBase = (
   req: Request<{ locationId: string }>,
 ) => Promise<LocationsIdGETRes>
 
-// 再参照等最小限にしているため
-// eslint-disable-next-line max-statements
 const locationsIdGetBase: TagsIdGetBase = async (req) => {
   // TODO: trace id を設定したら定義場所を変更
   const infoLoggerMain = loggerWrapper(infoLogger, { traceId: "-" })
@@ -23,32 +18,19 @@ const locationsIdGetBase: TagsIdGetBase = async (req) => {
   } = req
 
   const [photosRes, tagsRes, locationRes] = await Promise.all([
-    strapiApiClient.get<Photos>(`/photos?${Query}`),
-    strapiApiClient.get<Tags>(`/tags?${Query}`),
-    strapiApiClient.get<LocationsId>(`/locations/${locationId}?${Query}`),
+    strapiApiMethods.getPhotos(),
+    strapiApiMethods.getTags(),
+    strapiApiMethods.getLocationsId(locationId),
   ])
 
-  if (isErrorStatus(photosRes.status)) {
-    throw new Error(`photos status: ${photosRes.status}`)
-  }
-
-  if (isErrorStatus(tagsRes.status)) {
-    throw new Error(`tags status: ${tagsRes.status}`)
-  }
-
-  if (isErrorStatus(locationRes.status)) {
-    throw new Error(`tag status: ${locationRes.status}`)
-  }
-
   const getClientImageMain = getClientImage({
-    photos: photosRes.data.data,
-    tags: tagsRes.data.data,
+    photos: photosRes,
+    tags: tagsRes,
   })
 
   const response: LocationsIdGETRes = {
-    images:
-      locationRes.data.data.attributes.photos.data.map(getClientImageMain),
-    locationName: locationRes.data.data.attributes.locationName,
+    images: locationRes.attributes.photos.data.map(getClientImageMain),
+    locationName: locationRes.attributes.locationName,
   }
 
   infoLoggerMain({
