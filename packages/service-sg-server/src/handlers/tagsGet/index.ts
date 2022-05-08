bd1f0396dc19f1e620d9ba6d05ac4f8c86c7bb50
akky-xxxx <114.s.aki@gmail.com>
2022-05-08 21:19:57 +0900

    refactor(service-sg-server): replace to strapiApiMethods at tagsGet
    https://github.com/akky-xxxx/project-my-homepage/issues/92

--- packages/service-sg-server/src/handlers/tagsGet/index.ts
+++ packages/service-sg-server/src/handlers/tagsGet/index.ts
@@ -1,39 +1,26 @@
-import { Photos, Tags } from "common-types"
 import { TagsGETRes } from "../../libs/bffApiClient"
-import { Query } from "../../shared/const/strapi/Query"
 import { apiHandler } from "../../shared/utils/apiHandler"
 import { extractImagePath } from "../../shared/utils/extractImagePath"
-import { isErrorStatus } from "../../shared/utils/isErrorStatus"
 import { infoLogger, loggerWrapper } from "../../shared/utils/logger"
-import { strapiApiClient } from "../../shared/utils/strapiApiClient"
+import { strapiApiMethods } from "../../shared/utils/strapiApiMethods"
 import { getClientTag } from "./modules/getClientTag"
 
 type TagsGetBase = () => Promise<TagsGETRes>
 
-// 再参照等最小限にしているため
-// eslint-disable-next-line max-statements
 const tagsGetBase: TagsGetBase = async () => {
   // TODO: trace id を設定したら定義場所を変更
   const infoLoggerMain = loggerWrapper(infoLogger, { traceId: "-" })
 
   const [photosRes, tagsRes] = await Promise.all([
-    strapiApiClient.get<Photos>(`/photos?${Query}`),
-    strapiApiClient.get<Tags>(`/tags?${Query}`),
+    strapiApiMethods.getPhotos(),
+    strapiApiMethods.getTags(),
   ])
 
-  if (isErrorStatus(photosRes.status)) {
-    throw new Error(`photos status: ${photosRes.status}`)
-  }
-
-  if (isErrorStatus(tagsRes.status)) {
-    throw new Error(`tags status: ${tagsRes.status}`)
-  }
-
-  const getClientTagMain = getClientTag(extractImagePath(photosRes.data.data))
+  const getClientTagMain = getClientTag(extractImagePath(photosRes))
 
   // .filter(Boolean) で false を除去している
   // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-  const tags: TagsGETRes["tags"] = tagsRes.data.data
+  const tags: TagsGETRes["tags"] = tagsRes
     .map(getClientTagMain)
     .filter(Boolean) as TagsGETRes["tags"]
 
