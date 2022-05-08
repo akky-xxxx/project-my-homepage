3b0c302fc6dbedc27c8a2f448ec9d1ecef2a6edb
akky-xxxx <114.s.aki@gmail.com>
2022-05-08 21:15:24 +0900

    feat(service-sg-server): implement getPhotos of strapiApiMethods
    https://github.com/akky-xxxx/project-my-homepage/issues/92

---
+++ packages/service-sg-server/src/shared/utils/strapiApiMethods/modules/getPhotos/index.ts
@@ -0,0 +1,16 @@
+import { Photos } from "common-types"
+import { Query } from "../../../../const/strapi/Query"
+import { isErrorStatus } from "../../../isErrorStatus"
+import { strapiApiClient } from "../../../strapiApiClient"
+
+type GetPhotos = () => Promise<Photos["data"]>
+
+export const getPhotos: GetPhotos = async () => {
+  const { data, status } = await strapiApiClient.get<Photos>(`/photos?${Query}`)
+
+  if (isErrorStatus(status)) {
+    throw new Error(`getPhotos status: ${status}`)
+  }
+
+  return data.data
+}
