1a6b362e2f8bd9c1d39c3c9452a12dfef255a037
akky-xxxx <114.s.aki@gmail.com>
2022-05-14 23:12:03 +0900

    feat(service-client): define hasHover media query
    https://github.com/akky-xxxx/project-my-homepage/issues/101

--- packages/service-client/src/shared/const/MediaQuery/index.ts
+++ packages/service-client/src/shared/const/MediaQuery/index.ts
@@ -1,4 +1,6 @@
+/* eslint-disable sort-keys */
 export const MediaQuery = {
   Pc: "@media screen and (min-width: 1025px)",
   Tb: "@media screen and (max-width: 1024px)",
+  Hover: "@media screen and (hover: hover)",
 } as const
