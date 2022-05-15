1d5de11526f9d21e78d713cf6db1d44dae2c4b08
akky-xxxx <114.s.aki@gmail.com>
2022-05-15 15:14:20 +0900

    test(service-sg-server): fix tests for updated api
    https://github.com/akky-xxxx/project-my-homepage/issues/73
    https://github.com/akky-xxxx/project-my-homepage/issues/101

--- packages/service-sg-server/src/handlers/photoGalleryGet/index.test.ts
+++ packages/service-sg-server/src/handlers/photoGalleryGet/index.test.ts
@@ -1,6 +1,5 @@
 import { UnknownObject } from "common-types"
 import supertest, { Response } from "supertest"
-import { isObject } from "remeda"
 // node_modules を除き最初に import させるため
 // organize-imports-ignore
 import "../../modules/initialize"
@@ -12,7 +11,6 @@ import { spyResponse } from "./const/spy"
 const app = getServer()
 const request = supertest(app)
 
-const hyphenSyntaxYearMonthRegExp = /^\d{4}-\d{2}$/
 const slashSyntaxDateRegExp = /^\d{4}(?:\/\d{2}){2}$/
 
 describe("GET /api/photo-gallery", () => {
@@ -35,67 +33,80 @@ describe("GET /api/photo-gallery", () => {
       expect(res.status).toBe(200)
     })
 
-    it("cards プロパティがあり、配列である", () => {
+    it("images プロパティがあり、配列である", () => {
       expect(spy).toHaveBeenCalled()
-      expect(Array.isArray(res.body?.cards)).toBeTruthy()
+      expect(Array.isArray(res.body?.images)).toBeTruthy()
     })
 
-    it("cards[number] はオブジェクトである", () => {
+    it("images[number] はオブジェクトである", () => {
       expect(spy).toHaveBeenCalled()
-      if (!Array.isArray(res.body?.cards)) return
-      res.body.cards.forEach((card: unknown) => {
-        expect(typeof card).toBe("object")
-        expect(card).not.toBeNull()
+      if (!Array.isArray(res.body?.images)) return
+      res.body.images.forEach((image: unknown) => {
+        expect(typeof image).toBe("object")
+        expect(image).not.toBeNull()
       })
     })
 
-    it("cards[number].imagePath は http で始まる文字列である", () => {
+    it("images[number].imageId は文字列である", () => {
       expect(spy).toHaveBeenCalled()
-      res.body.cards.forEach((card: unknown) => {
-        if (typeof card !== "object" || card === null) return
-        const newCard: UnknownObject = { ...card }
+      res.body.images.forEach((image: unknown) => {
+        if (typeof image !== "object" || image === null) return
+        const newImage: UnknownObject = { ...image }
 
-        if (typeof newCard.imagePath !== "string") return
-        expect(newCard.imagePath.startsWith("http")).toBe(true)
+        expect(typeof newImage.imageId).toBe("string")
       })
     })
 
-    it("cards[number].location はオブジェクトであり、 locationId, locationName に文字列を持つ", () => {
+    it("images[number].imagePath は http で始まる文字列である", () => {
       expect(spy).toHaveBeenCalled()
-      res.body.cards.forEach((card: unknown) => {
-        if (typeof card !== "object" || card === null) return
-        const newCard: UnknownObject = { ...card }
+      res.body.images.forEach((image: unknown) => {
+        if (typeof image !== "object" || image === null) return
+        const newImage: UnknownObject = { ...image }
 
-        if (!newCard.location || !isObject(newCard.location)) return
-        const location = { ...newCard.location }
-        if (
-          typeof location.locationId !== "string" ||
-          typeof location.locationName !== "string"
-        )
-          return
-        expect(typeof location.locationId === "string").toBeTruthy()
-        expect(typeof location.locationName === "string").toBeTruthy()
+        if (typeof newImage.imagePath !== "string") return
+        expect(newImage.imagePath.startsWith("http")).toBe(true)
       })
     })
 
-    it("cards[number].takenAt はオブジェクトであり、 yearMonth, viewTakenAt に文字列を持つ", () => {
+    it("images[number].width は文字列である", () => {
       expect(spy).toHaveBeenCalled()
-      res.body.cards.forEach((card: unknown) => {
-        if (typeof card !== "object" || card === null) return
-        const newCard: UnknownObject = { ...card }
+      res.body.images.forEach((image: unknown) => {
+        if (typeof image !== "object" || image === null) return
+        const newImage: UnknownObject = { ...image }
 
-        if (!newCard.takenAt || !isObject(newCard.takenAt)) return
-        const takenAt = { ...newCard.takenAt }
-        if (
-          typeof takenAt.yearMonth !== "string" ||
-          typeof takenAt.viewTakenAt !== "string"
-        )
-          return
+        expect(typeof newImage.width).toBe("number")
+      })
+    })
 
-        expect(typeof takenAt.yearMonth === "string").toBeTruthy()
-        expect(hyphenSyntaxYearMonthRegExp.test(takenAt.yearMonth)).toBe(true)
-        expect(typeof takenAt.viewTakenAt === "string").toBeTruthy()
-        expect(slashSyntaxDateRegExp.test(takenAt.viewTakenAt)).toBe(true)
+    it("images[number].height は文字列である", () => {
+      expect(spy).toHaveBeenCalled()
+      res.body.images.forEach((image: unknown) => {
+        if (typeof image !== "object" || image === null) return
+        const newImage: UnknownObject = { ...image }
+
+        expect(typeof newImage.height).toBe("number")
+      })
+    })
+
+    it("images[number].locationName は文字列である", () => {
+      expect(spy).toHaveBeenCalled()
+      res.body.images.forEach((image: unknown) => {
+        if (typeof image !== "object" || image === null) return
+        const newImage: UnknownObject = { ...image }
+
+        expect(typeof newImage.locationName).toBe("string")
+      })
+    })
+
+    it("images[number].takenAt は文字列である", () => {
+      expect(spy).toHaveBeenCalled()
+      res.body.images.forEach((image: unknown) => {
+        if (typeof image !== "object" || image === null) return
+        const newImage: UnknownObject = { ...image }
+
+        expect(typeof newImage.takenAt).toBe("string")
+        if (typeof newImage.takenAt !== "string") return
+        expect(slashSyntaxDateRegExp.test(newImage.takenAt)).toBe(true)
       })
     })
   })
