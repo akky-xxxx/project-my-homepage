fd3aad42c77a82ace75258f737d8116bc5961231
akky-xxxx <114.s.aki@gmail.com>
2022-05-15 16:47:20 +0900

    feat(service-sg-server): use isMainVisual
    https://github.com/akky-xxxx/project-my-homepage/issues/101

--- packages/service-sg-server/src/handlers/homeGet/index.ts
+++ packages/service-sg-server/src/handlers/homeGet/index.ts
@@ -3,6 +3,7 @@ import { apiHandler } from "../../shared/utils/apiHandler"
 import { infoLogger, loggerWrapper } from "../../shared/utils/logger"
 import { strapiApiMethods } from "../../shared/utils/strapiApiMethods"
 import { getMainVisualPath } from "./modules/getMainVisualPath"
+import { isMainVisual } from "./modules/isMainVisual"
 
 type ResponseBody = {
   mainVisualPaths: string[]
@@ -13,7 +14,7 @@ type HomeGetBase = () => Promise<HomeGETRes>
 const homeGetBase: HomeGetBase = async () => {
   const photos = await strapiApiMethods.getPhotos()
 
-  const mainVisualPaths = photos.map(getMainVisualPath)
+  const mainVisualPaths = photos.filter(isMainVisual).map(getMainVisualPath)
   const infoLoggerMain = loggerWrapper(infoLogger, { traceId: "-" })
 
   const response: ResponseBody = {
