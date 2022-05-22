856ec8318531ca65324e7750ed7de06d87094ec7
akky-xxxx <114.s.aki@gmail.com>
2022-05-22 19:03:49 +0900

    feat(service-sg-server): implement getHttpUrl
    https://github.com/akky-xxxx/project-my-homepage/issues/111

---
+++ packages/service-sg-server/src/shared/utils/getHttpUrl/index.test.ts
@@ -0,0 +1,13 @@
+// organize-imports-ignore
+import "../../../modules/initialize"
+import { getHttpUrl } from "."
+
+describe("getHttpUrl", () => {
+  it.each([
+    ["http://hoge.com", "http://hoge.com"],
+    ["https://hoge.com", "https://hoge.com"],
+    ["/test", `${process.env.STRAPI_SERVER || ""}/test`],
+  ] as const)("引数が %s の時 %s を返す", (argument, expectedValue) => {
+    expect(getHttpUrl(argument)).toBe(expectedValue)
+  })
+})
