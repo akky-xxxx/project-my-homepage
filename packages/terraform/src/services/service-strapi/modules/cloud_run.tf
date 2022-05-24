eea4314b6d059a8f63dbf56f793808cc2f0a67b3
akky-xxxx <114.s.aki@gmail.com>
2022-05-24 22:34:14 +0900

    feat(terraform): split strapi code to new files
    https://github.com/akky-xxxx/project-my-homepage/issues/59

--- packages/terraform/src/services/service-strapi/modules/cloud_run.tf
+++ packages/terraform/src/services/service-strapi/modules/cloud_run.tf
@@ -1,39 +1,39 @@
 resource "google_cloud_run_service" "strapi" {
-  name     = lookup(var.run_strapi, "name")
-  location = var.region
+  name     = var.cloud_run_service_name
+  location = "asia-northeast1"
 
   template {
     spec {
-      container_concurrency = lookup(var.run_strapi, "concurrency")
+      container_concurrency = var.cloud_run_container_concurrency
 
       containers {
-        image = lookup(var.run_strapi, "registry")
+        image = var.cloud_run_image_name
 
         resources {
           limits = {
-            memory = lookup(var.run_strapi, "memory")
-            cpu : lookup(var.run_strapi, "cpu")
+            memory = var.cloud_run_memory
+            cpu : var.cloud_run_cpu
           }
         }
 
         env {
           name  = "APP_KEYS"
-          value = lookup(var.run_strapi, "app_keys")
+          value = var.cloud_run_api_keys
         }
 
         env {
           name  = "JWT_SECRET"
-          value = lookup(var.run_strapi, "jwt_secret")
+          value = var.cloud_run_jwt_secret
         }
 
         env {
           name  = "API_TOKEN_SALT"
-          value = lookup(var.run_strapi, "api_token_salt")
+          value = var.cloud_run_api_token_salt
         }
 
         env {
           name  = "ADMIN_JWT_SECRET"
-          value = lookup(var.run_strapi, "admin_jwt_secret")
+          value = var.cloud_run_admin_jwt_secret
         }
 
         env {
@@ -78,31 +78,4 @@ resource "google_cloud_run_service_iam_policy" "no_auth-strapi" {
   service  = google_cloud_run_service.strapi.name
 
   policy_data = data.google_iam_policy.no_auth.policy_data
-}
-
-resource "google_storage_bucket" "strapi" {
-  name          = lookup(var.storage_strapi, "name")
-  location      = lookup(var.storage_strapi, "location")
-  force_destroy = true
-}
-
-resource "google_sql_database_instance" "db_instance-strapi" {
-  name             = "strapi-db-instance-${random_id.id.hex}"
-  region           = "us-central1"
-  database_version = lookup(var.db_strapi, "version")
-
-  settings {
-    tier = "db-f1-micro"
-  }
-}
-
-resource "google_sql_database" "db-strapi" {
-  name     = lookup(var.db_strapi, "name")
-  instance = google_sql_database_instance.db_instance-strapi.name
-}
-
-resource "google_sql_user" "users" {
-  name     = lookup(var.db_strapi, "user")
-  password = lookup(var.db_strapi, "pass")
-  instance = google_sql_database_instance.db_instance-strapi.name
 }
--- packages/terraform/src/variables.tf
+++ packages/terraform/src/variables.tf
@@ -4,8 +4,3 @@ variable "project_id" {}
 
 variable "run_client" {}
 variable "run_sg_server" {}
-variable "run_strapi" {}
-
-variable "storage_strapi" {}
-
-variable "db_strapi" {}
