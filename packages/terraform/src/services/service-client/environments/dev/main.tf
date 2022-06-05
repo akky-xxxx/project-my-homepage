a8b6bd448a1de556d0d04475426c0886429aebdc
akky-xxxx <114.s.aki@gmail.com>
2022-06-05 20:31:04 +0900

    refactor(terraform): sort order of variables for cloud run
    https://github.com/akky-xxxx/project-my-homepage/issues/152

--- packages/terraform/src/services/service-client/environments/dev/main.tf
+++ packages/terraform/src/services/service-client/environments/dev/main.tf
@@ -24,11 +24,11 @@ module "modules" {
   random_id = random_id.id.hex
 
   # cloud run
-  cloud_run_service_name          = "${lookup(var.run_client, "name")}-dev"
   cloud_run_container_concurrency = "100"
-  cloud_run_registry              = lookup(var.run_client, "registry")
-  cloud_run_memory                = "16Gi"
   cloud_run_cpu                   = "4000m"
   cloud_run_max_scale             = "50"
+  cloud_run_memory                = "16Gi"
+  cloud_run_registry              = lookup(var.run_client, "registry")
+  cloud_run_service_name          = "${lookup(var.run_client, "name")}-dev"
   cloud_run_sg_server_server      = var.run_sg_server
 }
