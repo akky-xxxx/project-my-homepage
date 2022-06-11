e393ed72c2143c015bda8c5c7daf50e9c035b6fe
akky-xxxx <114.s.aki@gmail.com>
2022-06-11 16:11:31 +0900

    feat(service-client): define title element
    https://github.com/akky-xxxx/project-my-homepage/issues/166

--- packages/service-client/src/pages/_app.page.tsx
+++ packages/service-client/src/pages/_app.page.tsx
@@ -6,15 +6,20 @@ import { Layout } from "../components/layouts/Layout"
 import { MenuData } from "../shared/const/MenuData"
 import { GlobalStyle } from "../shared/styles/globalStyle"
 import { getCurrentPagePosition } from "./modules/getCurrentPagePosition"
+import { getPageName } from "./modules/getPageName"
 
 import type { AppProps } from "next/app"
 
 const MyApp = ({ Component, pageProps, router }: AppProps) => {
   const { route } = router
+  const pageTitle = `${getPageName(route)}${
+    process.env.NEXT_PUBLIC_TITLE || ""
+  }`
 
   return (
     <Fragment>
       <Head>
+        <title>{pageTitle}</title>
         {process.env.NEXT_PUBLIC_PRODUCTION_ENVIRONMENT !== "true" && (
           <meta name="robots" content="noindex nofollow" />
         )}
