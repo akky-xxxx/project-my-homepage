62adddd10cef0a5966ff4a16063b02b33c5ebf41
akky-xxxx <114.s.aki@gmail.com>
2022-06-13 00:19:08 +0900

    chore(common-icons): move stories to under the spec
    https://github.com/akky-xxxx/project-my-homepage/issues/178

--- packages/common-icons/spec/stories/Info.stories.tsx
+++ packages/common-icons/spec/stories/Info.stories.tsx
@@ -1,6 +1,6 @@
 import { ComponentStoryObj, Meta } from "@storybook/react"
 import { ComponentProps } from "react"
-import { Info } from "./index"
+import { Info } from "../../src"
 
 type InfoType = typeof Info
 type InfoStory = ComponentStoryObj<InfoType>
