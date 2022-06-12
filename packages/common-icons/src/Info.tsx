d1b64257b12156731aff0c2e0b4978736e6f428b
akky-xxxx <114.s.aki@gmail.com>
2022-06-13 00:36:05 +0900

    refactor(common-icons): change to destructuring assignment
    https://github.com/akky-xxxx/project-my-homepage/issues/179

--- packages/common-icons/src/Info.tsx
+++ packages/common-icons/src/Info.tsx
@@ -2,13 +2,17 @@ import React from "react"
 
 import type { SVGProps } from "react";
 
-export const Info = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 50 50"
-    fill={props.color}
-    {...props}
-  >
-    <path d="M25 2C12.297 2 2 12.297 2 25s10.297 23 23 23 23-10.297 23-23S37.703 2 25 2zm0 9a3 3 0 1 1 0 6 3 3 0 0 1 0-6zm4 27h-8v-2h2V23h-2v-2h6v15h2v2z" />
-  </svg>
-)
+export const Info = (props: SVGProps<SVGSVGElement>) => {
+  const { color } = props
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 50 50"
+      fill={color}
+      {...props}
+    >
+      <path d="M25 2C12.297 2 2 12.297 2 25s10.297 23 23 23 23-10.297 23-23S37.703 2 25 2zm0 9a3 3 0 1 1 0 6 3 3 0 0 1 0-6zm4 27h-8v-2h2V23h-2v-2h6v15h2v2z" />
+    </svg>
+  )
+}
