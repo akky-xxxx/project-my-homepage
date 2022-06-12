b0236a6b8d4c381cbad5bac05c4ace89bffca2ba
akky-xxxx <114.s.aki@gmail.com>
2022-06-13 00:54:10 +0900

    chore(root): define config for eslint-plugin-storybook
    https://github.com/akky-xxxx/project-my-homepage/issues/179

---
+++ configs/eslint/storybook/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: ["storybook"],
+  extends: [
+    "plugin:storybook/recommended",
+    "plugin:storybook/addon-interactions",
+    "plugin:storybook/csf-strict",
+    "plugin:storybook/csf",
+  ],
+}
