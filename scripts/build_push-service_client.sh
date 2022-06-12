863458a53c1d20cb42b967c1f5bb6d4bd7e7a329
akky-xxxx <114.s.aki@gmail.com>
2022-06-12 21:26:02 +0900

    chore(root): add new line for readable
    https://github.com/akky-xxxx/project-my-homepage/issues/174

--- scripts/build_push-service_client.sh
+++ scripts/build_push-service_client.sh
@@ -6,5 +6,8 @@ SERVICE_NAME=service-client
 REPOSITORY=$REGION-docker.pkg.dev/$PRODUCTION_ID/cloud-run-source-deploy/$SERVICE_NAME:latest
 
 # 要 gcloud auth configure-docker $REGION-docker.pkg.dev の実行
-docker build --build-arg NEXT_PUBLIC_SG_SERVER="$NEXT_PUBLIC_SG_SERVER" -f packages/"$SERVICE_NAME"/Dockerfile -t $REPOSITORY .
+docker build \
+  --build-arg NEXT_PUBLIC_SG_SERVER="$NEXT_PUBLIC_SG_SERVER" \
+  -f packages/"$SERVICE_NAME"/Dockerfile \
+  -t "$REPOSITORY" .
 docker push "$REPOSITORY"
