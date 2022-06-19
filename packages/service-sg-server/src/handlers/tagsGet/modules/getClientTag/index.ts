458480ae5555a6ef88cdc9d7037a00ae98c4ec26
akky-xxxx <114.s.aki@gmail.com>
2022-06-19 21:24:04 +0900

    refactor(service-sg-server): fix getClientTag
    https://github.com/akky-xxxx/project-my-homepage/issues/44

--- packages/service-sg-server/src/handlers/tagsGet/modules/getClientTag/index.ts
+++ packages/service-sg-server/src/handlers/tagsGet/modules/getClientTag/index.ts
@@ -2,15 +2,13 @@ import { extractPhotoId } from "../../../../shared/utils/extractPhotoId"
 import { getRandomImage } from "../../../../shared/utils/getRandomImage"
 
 import type { TagsGETRes } from "../../../../libs/bffApiClient"
-import type { ExtractImagePathMain } from "../../../../shared/utils/extractImage"
+import type { ExtractImageMain } from "../../../../shared/utils/extractImage"
 import type { Tags } from "common-types"
 
 type GetClientTagMain = (
   tag: Tags["data"][number],
 ) => TagsGETRes["tags"][number] | false
-type GetClientTag = (
-  extractImagePathMain: ExtractImagePathMain,
-) => GetClientTagMain
+type GetClientTag = (extractImagePathMain: ExtractImageMain) => GetClientTagMain
 
 export const getClientTag: GetClientTag = (extractImagePathMain) => (tag) => {
   const {
@@ -24,8 +22,8 @@ export const getClientTag: GetClientTag = (extractImagePathMain) => (tag) => {
   if (!photos.length) return false
 
   const targetPhotoIdList = photos.map(extractPhotoId)
-  const imagePaths = targetPhotoIdList.map(extractImagePathMain)
-  const imagePath = getRandomImage(imagePaths)
+  const imageInfoList = targetPhotoIdList.map(extractImagePathMain)
+  const { url: imagePath } = getRandomImage(imageInfoList)
 
   return {
     imageNum: photos.length,
