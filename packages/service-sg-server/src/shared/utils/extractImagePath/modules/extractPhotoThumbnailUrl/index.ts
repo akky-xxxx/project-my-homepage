a1d533d7bde989ccb8846ba75c83110efecc1a3e
akky-xxxx <114.s.aki@gmail.com>
2022-06-19 20:50:09 +0900

    feat(service-sg-server): fix extractPhotoInfo
    https://github.com/akky-xxxx/project-my-homepage/issues/44

--- packages/service-sg-server/src/shared/utils/extractImagePath/modules/extractPhotoThumbnailUrl/index.ts
+++
@@ -1,6 +0,0 @@
-import type { Photos } from "common-types"
-
-type ExtractPhotoThumbnailUrl = (photo: Photos["data"][number]) => string
-
-export const extractPhotoThumbnailUrl: ExtractPhotoThumbnailUrl = (photo) =>
-  photo.attributes.photo.data.attributes.formats.thumbnail.url
